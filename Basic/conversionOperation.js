4e0089b5bf48d502f9cca4150a6bfd9a50e99304
perveshgoyal <50743265+perveshgoyal@users.noreply.github.com>
2024-06-13 17:30:09 +0000

    conversionOperation

--- Basic/conversionOperation.js
+++ Basic/conversionOperation.js
@@ -7,14 +7,36 @@
 //"33" => 33
 //"33abc" => NaN
 
-let isLoggedin = ""
-let booleanIsLoggesIn = Boolean(isLoggedin)
-console.log(booleanIsLoggesIn)
+// let isLoggedin = ""
+// let booleanIsLoggesIn = Boolean(isLoggedin)
+// console.log(booleanIsLoggesIn)
 
 // 1=>true
 //"" => false
 //"per"=>true
 
-let sNum = 33
-let stringNumber = String(sNum)
-console.log(stringNumber)
+// let sNum = 33
+// let stringNumber = String(sNum)
+// console.log(stringNumber)
+
+
+//*****************************Operations**************************/
+let value = 3
+let negValue = -value
+//console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+let str1 = "hello"
+let str2 = " me"
+let str3 = str1 + str2
+// console.log(str3)
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(+true)
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
