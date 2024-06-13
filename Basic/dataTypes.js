7f38ffa173c917aa4cda46e3fbfcbedadc349b70
perveshgoyal <50743265+perveshgoyal@users.noreply.github.com>
2024-06-13 16:35:30 +0000

    dataTypes

---
+++ Basic/dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";//treat all js code as newer version
+//alert("Helo")
+let name = "Pervsh"
+let age = 18
+let isLoggedin = false
+//number
+//bigint
+//string
+//boolean
+//null=>standalone vaue
+//unidentified
+//symbol=>uniqueness
+//object
+console.log(typeof null)
+console.log(typeof undefined)
+
