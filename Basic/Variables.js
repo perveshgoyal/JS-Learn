89428742020a3496c292553a97273946773a6358
perveshgoyal <50743265+perveshgoyal@users.noreply.github.com>
2024-06-13 16:55:14 +0000

    conversionOperation

---
+++ Basic/Variables.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accountEmail = "pervesh@gmail.com"
+var accontPassword = "12345"
+accountCity = "Noida"
+let accountState;
+
+// accountId = 2
+accountEmail = "per@gmail.com"
+accontPassword = "123"
+accountCity = "Delhi"
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accontPassword,accountState])
---
+++ Basic/conversionOperation.js
@@ -0,0 +1,20 @@
+//let score = "per"
+//console.log(typeof score)
+//console.log(typeof(score))
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+//"33" => 33
+//"33abc" => NaN
+
+let isLoggedin = ""
+let booleanIsLoggesIn = Boolean(isLoggedin)
+console.log(booleanIsLoggesIn)
+
+// 1=>true
+//"" => false
+//"per"=>true
+
+let sNum = 33
+let stringNumber = String(sNum)
+console.log(stringNumber)
